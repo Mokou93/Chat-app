508f21744f0f6bc30765cba20636d511e8fb74cb
Koullou Mohamed <koullou.mohamed@gmail.com>
2019-10-15 09:55:12 +0200

    fin exercice React2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Contact from './components/Contact';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Contact name="Mokou" avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRKfyE41eRKMZEor9-mWCCfaVbbIQYIjiZGbyrmWGwTi5BzqE5A" status={true} />
+   <Contact name="Tracey Rose" avatar="https://randomuser.me/api/portraits/women/86.jpg" status={false} />
+   <Contact name="Gertrude Lopez" avatar="https://randomuser.me/api/portraits/women/96.jpg" status={true} />
     </div>
   );
 }
