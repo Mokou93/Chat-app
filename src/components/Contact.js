6ccab9655ff8e4dc98097f5a5586f56445968ebb
Koullou Mohamed <koullou.mohamed@gmail.com>
2019-10-30 18:36:55 +0100

    ajout d'un stateful, au click changement du status online/offline prédéfinis, par son contraire

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,42 @@
 import React from "react";
 import "./contact.css";
 
+//CLASS COMPONENTS
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { status: props.status };
+  }
+
+  clickChangeStatus = event => {
+    const newStatus = !this.state.status;
+    this.setState({ status: newStatus });
+  };
+  render() {
+    return (
+      <div>
+        <div className="Contact">
+          <img alt="avatar" className="avatar" src={this.props.avatar}></img>
+          <div className="status" onClick={this.clickChangeStatus}>
+            <h4>{this.props.name}</h4>
+            <p className="status-text">
+              <span
+                className={
+                  this.state.status ? "status-online" : "status-offline"
+                }
+              ></span>
+              {this.state.status ? "online" : "offline"}
+              {""}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Contact;
+
+/*COMPOSANT AVEC FONCTION
 function Contact(props) {
   return (
     <div>
@@ -19,4 +55,4 @@ function Contact(props) {
     </div>
   );
 }
-export default Contact;
+export default Contact;*/
